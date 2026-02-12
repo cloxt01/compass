4e27b4c8df547768bf2f62fff94bfa742647c1dc
Nacho <ferdi.cloxt00@gmail.com>
2026-02-12 20:30:38 +0700

    fix: provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,16 +29,6 @@ class AppServiceProvider extends ServiceProvider
          if (config('app.env') !== 'local') {
             URL::forceScheme('https');
         }
-        // Ensure `routes/api.php` is loaded (project didn't have RouteServiceProvider)
-        if (file_exists(base_path('routes/api.php'))) {
-            Route::prefix('api-v1')
-                ->middleware('api')
-                ->group(base_path('routes/api.php'));
-        }
-        if (file_exists(base_path('routes/web.php'))) {
-            Route::group(['middleware' => ['web']], function () {
-                require base_path('routes/web.php');
-            });
-        }
+       
     }
 }
