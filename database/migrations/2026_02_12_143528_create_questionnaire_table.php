4248a6298bb365d18671bd307efc02c5d8a7f92f
Nacho <ferdi.cloxt00@gmail.com>
2026-02-12 21:57:47 +0700

    fix: check questionnaire before create

--- database/migrations/2026_02_12_143528_create_questionnaire_table.php
+++ database/migrations/2026_02_12_143528_create_questionnaire_table.php
@@ -4,20 +4,31 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up(): void {
-        Schema::create('questionnaire', function (Blueprint $table) {
-            $table->increments('id'); // Ini otomatis int(11) NOT NULL AUTO_INCREMENT
-            $table->string('question_id');
-            $table->string('question_text');
-            $table->string('answer_id')->nullable();
-            $table->string('answer_text')->nullable();
-            $table->string('last_answer')->nullable();
-            $table->longText('options')->nullable();
-        });
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('questionnaire')) {
+            Schema::create('questionnaire', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('question_id');
+                $table->string('question_text');
+                $table->string('answer_id')->nullable();
+                $table->string('answer_text')->nullable();
+                $table->string('last_answer')->nullable();
+                $table->longText('options')->nullable();
+            });
+        }
     }
 
-    public function down(): void {
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
         Schema::dropIfExists('questionnaire');
     }
 };
