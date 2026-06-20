c97a2f785c4d7e3e6c1eb7ba74cf69f91d58b7f4
cloxt01 <ferdi.cloxt00@gmail.com>
2026-06-21 01:31:58 +0700

    fix: typo route

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::prefix('apply')->middleware(['web','auth', 'token'])->group(function() {
 Route::post('/{provider}/search-location', [\App\Http\Controllers\Api\Glints\api::class, 'searchLocation'])->name('api.search.location');
 Route::prefix('platform')->group(function() {
     Route::post('/{provider}/search-location', [PlatformController::class, 'locationInfo'])->name('api.platform.locationInfo');
-    Route::post('/{provider}/login}', [PlatformController::class, 'login'])->name('api.platform.login');
+    Route::post('/{provider}/login', [PlatformController::class, 'login'])->name('api.platform.login');
     Route::post('/{provider}/passwordless-login', [PlatformController::class, 'passwordless_login'])->name('api.platform.passwordless-login');
     Route::post('/{provider}/verify-otp', [PlatformController::class, 'verify_otp'])->name('api.platform.verify-otp');
 });
