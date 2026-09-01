45ac8a9eb59180e74c851a7837493d9a267362b9
D' Manuver <ferdi.cloxt01@gmail.com>
2026-09-01 10:54:09 +0700

    update: app js to fix alpine

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,9 @@
 import './bootstrap'
 import './sidebar'
 
-import Alpine from 'alpinejs'
-import intersect from '@alpinejs/intersect'
-
 import { createIcons, icons } from 'lucide'
 
-// Alpine
-window.Alpine = Alpine
-Alpine.plugin(intersect)
-
-// Alpine components
 import glintsSearchLocation from './alpine/glints-search-location.js'
-
-Alpine.data('glintsSearchLocation', glintsSearchLocation)
-
-// Start Alpine
-Alpine.start()
 
 // Lucide
 window.lucide = {
